31c3bceab9e2477d578a7f89161b6feb2b242daf
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-09-24 11:06:43 +0530

    jenkinsfile

---
+++ milestone.py
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Automatically checks out your new milestone repository code
+                git url: 'https://github.com/KonankiPranavi/jenkins_milestone1.git', branch: 'main'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Compiling app.py...'
+                // Waits for 20 seconds before proceeding
+                sleep time: 20, unit: 'SECONDS'
+                // First milestone check
+                milestone(1)
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                // Milestone 2 at the very start of deployment
+                milestone(2)
+                echo 'Deploying application to production environment...'
+            }
+        }
+    }
+}
